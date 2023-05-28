b6308ef1126daf90f8fbbe4f50fb7c9395d8a0c4
Pedro <pedro.enrique1599@icloud.com>
2023-05-28 09:57:00 -0300

    initial migrations + database creation

--- BankAPII/Program.cs
+++ BankAPII/Program.cs
@@ -27,7 +27,7 @@ namespace BankAPII
                     client.Id = Convert.ToInt32(Console.ReadLine());
                     Console.Write("Type the client Name: ");
                     client.Name = Console.ReadLine();
-                    // clientRepository.Save(client);
+                    clientRepository.Save(client);
                     Console.WriteLine();
                     Console.WriteLine("Create another client?\nType '1' for yes.\nType '2' for no.");
                     Console.Write("Type your choice: ");
@@ -44,16 +44,13 @@ namespace BankAPII
             }
             #endregion
 
-            // Client cliente1 = new Client(12345678901, "João");
-            // Client cliente2 = new Client(98765432109, "Maria");
-
             // Criação de contas correntes
             BankAccount conta1 = new BankAccount(client);
             // BankAccount conta2 = new BankAccount(cliente2);
 
             // Realização de transações
-            conta1.Deposit(1000);
-            conta1.Withdraw(5000);
+            // conta1.Deposit(1000);
+            // conta1.Withdraw(5000);
             // conta1.Transfer(conta2, 500);
 
             // Exibição dos saldos
